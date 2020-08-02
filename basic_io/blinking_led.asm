2b1c9632d198346cf5e45c37e4c0e5b349bb0442
Ludiminium <ludiminium@gmail.com>
2020-08-03 00:20:24 +0200

    Update blinking led

--- basic_io/blinking_led.asm
+++ basic_io/blinking_led.asm
@@ -1,26 +1,27 @@
-.ORG   0x0000		  ; Next instruction to be written at 0x0000 
-RJMP   main				; Reset vector
+.ORG   0x0000                   ; Next instruction to be written at 0x0000 
+RJMP   main                     ; Reset vector
 
 main:
-	SBI DDRB, 5 		; Set Data Direction Register B to output for pin 13
+	LDI r16, 0xFF				; Fill register 16 with bits
+	OUT DDRB, r16				; Set Data Direction Register B to output for all pins
 
 loop:
-	SBI PortB, 5		; Set bit on pin 13
-	RCALL delay			; Relative call to delay of half a second
-	CBI PortB, 5		; Clear bit on pin 13
+	SBI PortB, 5				; Set bit on pin 13
+	RCALL delay					; Relative call to delay of half a second
+	CBI PortB, 5				; Clear bit on pin 13
 	RCALL delay
-	RCALL loop			; Relative jump to loop
+	RJMP loop					; Relative jump to loop
 
 delay:
-	LDI r16, 31			; Load r1 with 31
+	LDI r16, 31					; Load r1 with 31
 
-outer_loop:			    ; Store 1021 = 0b1111111101 in two registers r24:r25
-	LDI r24, low(1021)	; Load the lower bit series into r24 = 11101
-	LDI r25, high(1021)	; Load the upper bit series into r25 = 11111
+outer_loop:						; Store 1021 = 0b1111111101 in two registers r24:r25
+	LDI r24, low(1021)			; Load the lower bit series into r24 = 11101
+	LDI r25, high(1021)			; Load the upper bit series into r25 = 11111
 
 delay_loop:
-	ADIW r24:r25, 1		; Increase r24:r25 with 1
-	BRNE delay_loop		; If r24:r25 is not 0 (overflow), goto delay_loop
-	DEC r16				; Decrement r16 with 1
-	BRNE outer_loop		; If r16 is not 0, goto outer_loop
-	RET					; Returns to relative call
+	ADIW r24:r25, 1				; Increase r24:r25 with 1
+	BRNE delay_loop				; If r24:r25 is not 0 (overflow), goto delay_loop
+	DEC r16						; Decrement r16 with 1
+	BRNE outer_loop				; If r16 is not 0, goto outer_loop
+	RET							; Returns to relative call
